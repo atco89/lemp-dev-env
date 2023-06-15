146ccd681b2e7c86d1e94acaa49f51ef6816c8d8
Aleksandar Rakić <aleksandar.rakic@yahoo.com>
2023-06-15 08:52:29 +0200

    [2023-06-15] - work in progress.

--- bash/git-push.sh
+++ bash/git-push.sh
@@ -3,13 +3,13 @@
 # shellcheck disable=SC2039
 read -r -p "Are you sure you want to perform this action? [y/N]" response
 if [ "$response" = "y" ]; then
-  counter="$(git log -1 --pretty=%B | grep -o -E '[0-9]+')"
-  # shellcheck disable=SC2034
-  message=$(printf "#%s - work in progress." $((counter + 1)))
+  # shellcheck disable=SC2046
+  message=$(printf "[%s] - work in progress." $(date +'%Y-%m-%d'))
 
   git config user.name "Aleksandar Rakić"
   git config user.email "aleksandar.rakic@yahoo.com"
   git add .
-  git commit -m "work in progress 1"
+  git commit -m "$message"
+
   git push "git@github.com:atco89/lemp-dev-env.git"
 fi
